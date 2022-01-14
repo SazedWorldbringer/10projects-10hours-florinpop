b3e555adb4753c4d9abfa855663b93e264d0433f
Atharva Pardeshi <atharva.exe@gmail.com>
2022-01-14 18:43:19 +0530

    Finished countdown-timer project, added empty README.md file

---
+++ countdown-timer/script.js
@@ -0,0 +1,38 @@
+const secondsEl = document.getElementById('seconds')
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minutesEl = document.getElementById('minutes')
+
+const newYear = '1 Jan 2023'
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time
+}
+
+function countDown() {
+    const newYearsDate = new Date(newYear)
+    const currentDate = new Date()
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000
+
+    const seconds = Math.floor(totalSeconds) % 60
+    const days = Math.floor(totalSeconds / 3600 / 24)
+    const hours = Math.floor(totalSeconds / 3600) % 24
+    const minutes = Math.floor(totalSeconds / 60) % 60
+
+    secondsEl.innerHTML = formatTime(seconds)
+    daysEl.innerHTML = days
+    hoursEl.innerHTML = formatTime(hours)
+    minutesEl.innerHTML = formatTime(minutes)
+}
+
+countDown()
+
+setInterval(countDown, 1000)
+
+// const date = document.getElementById('date')
+// const defaultVal = date.defaultValue
+// const currentVal = date.value
+
+
+// console.log(defaultVal, currentVal)
